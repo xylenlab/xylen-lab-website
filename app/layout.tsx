255dee5dcc939b9b04a99f99ca1dd68d0a304ff7
xylenlab <info@xylenlab.com>
2025-06-08 21:28:15 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
